cc44a050c7a7228db7940ee31199731c8592ba99
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-03-14 16:21:50 -0400

    feat: remove hello world home page render

--- api/app.js
+++ api/app.js
@@ -22,9 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(assetsPath));
 
 // Route level middlewares
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
 app.use("/posts", postRouter);
 app.use("/", indexRouter);
 
