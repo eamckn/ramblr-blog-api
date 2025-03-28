0dc8c8b6cce38cd2eb4081e07982952d54ca7b07
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-03-28 18:35:35 -0400

    fix: change body parser from urlencoded to json

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // App level middlewares
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(assetsPath));
 app.use(cors());
 
