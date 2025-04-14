8ea292c013ff05ee36f91c9932b244995f6d4d2c
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2025-04-14 12:59:10 -0400

    refactor: remove unused app level middleware

--- app.js
+++ app.js
@@ -3,24 +3,17 @@ import "dotenv/config";
 import "./config/passport.js";
 import express from "express";
 import cors from "cors";
-import path from "node:path";
 import postRouter from "./routes/postRouter.js";
 import indexRouter from "./routes/indexRouter.js";
 
 // App constants
 const PORT = process.env.PORT || 8080;
-const __dirname = path.resolve();
-const assetsPath = path.join(__dirname, "public");
 
 // App initializations
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 // App level middlewares
 app.use(express.json());
-app.use(express.static(assetsPath));
 app.use(cors());
 
 // Route level middlewares
